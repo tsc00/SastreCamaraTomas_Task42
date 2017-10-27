6a614015d8c55a17ad54be28d4293633c8445690
Tomas <tsc00@iesemilidarder.com>
2017-10-27 20:46:58 +0200

    1

---
+++ src/TestRoad.java
@@ -0,0 +1,7 @@
+package transport;
+
+public class TestRoad {
+    static public void main (String args []) {
+        Road myRoad = new Road();
+        System.out.println();
+}
--- src/transport/Road.java
+++ src/transport/Road.java
@@ -1,4 +1,11 @@
 package transport;
 
 public class Road {
+    private Vehicle[] vehicles;
+    private  int[] numberOfVehicles;
+
+    public Road() {
+    public double addVehicle();
+    public int getNumberOfVehicles();
+    }
 }
