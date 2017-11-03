cb248553a97726e92a6a964c2c15ae86c02f7a14
Tomas <tsc00@iesemilidarder.com>
2017-11-03 20:30:43 +0100

    Final menos los comentarios

--- src/TestRoad.java
+++ src/TestRoad.java
@@ -1,7 +1,23 @@
 package transport;
 
+import javax.swing.plaf.synth.SynthEditorPaneUI;
+
 public class TestRoad {
-    static public void main (String args []) {
+    static public void main(String args[]) {
         Road myRoad = new Road();
-        System.out.println();
+        myRoad.addVehicle(10000);
+        myRoad.addVehicle(5000);
+        myRoad.addVehicle(4000);
+        myRoad.addVehicle(20000);
+        myRoad.addVehicle(15000);
+        myRoad.addVehicle(4000);
+
+        Vehicle Truck = myRoad.getVehicle(4);
+        boolean peso = Truck.addBox(10000);
+        System.out.println(peso);
+        peso = Truck.addBox(6000);
+        System.out.println(peso);
+
+        System.out.println("The total truck load number 4 is : " + Truck.getLoad() +  " KG");
+    }
 }
--- src/transport/Road.java
+++ src/transport/Road.java
@@ -1,11 +1,39 @@
 package transport;
 
 public class Road {
-    private Vehicle[] vehicles;
-    private  int[] numberOfVehicles;
+    private Vehicle[] Vehicles;
+    private int numberOfVehicles;
 
     public Road() {
-    public double addVehicle();
-    public int getNumberOfVehicles();
+        this.Vehicles = new Vehicle[5];
+        numberOfVehicles = 0;
+
+    }
+
+    public boolean addVehicle(double MaxLoad) {
+        if (numberOfVehicles < this.Vehicles.length) {
+            Vehicles[numberOfVehicles] = new Vehicle(MaxLoad);
+            numberOfVehicles++;
+
+                System.out.println("Creating a truck with " + MaxLoad + " Kg capacity");
+                return true;
+        } else {
+            System.out.println("ERROR: THIS ROAD CAN ONLY LOAD 5 VEHICLES");
+            return false;
+        }
+    }
+
+    public int getNumberOfVehicles() {
+        return numberOfVehicles;
+    }
+
+    public Vehicle getVehicle(int i) {
+        if (i < Vehicles.length) {
+            return Vehicles[i];
+        }
+        else {
+            return null;
+        }
     }
 }
+
