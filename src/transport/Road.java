2da099bd52c49f1e9be28a159a244b38a7794887
Tomás Sastre <32461851+tsc00@users.noreply.github.com>
2017-11-08 19:57:57 +0100

    Update Road.java

--- src/transport/Road.java
+++ src/transport/Road.java
@@ -1,15 +1,22 @@
 package transport;
+/**
+Creacion de la Clase Road almacenado en el package transport
+*/
 
 public class Road {
     private Vehicle[] Vehicles;
     private int numberOfVehicles;
-
+/**
+Creacion del constructor Road
+*/
     public Road() {
         this.Vehicles = new Vehicle[5];
         numberOfVehicles = 0;
 
     }
-
+/**
+Creacion del metodo addVehicle
+*/
     public boolean addVehicle(double MaxLoad) {
         if (numberOfVehicles < this.Vehicles.length) {
             Vehicles[numberOfVehicles] = new Vehicle(MaxLoad);
@@ -22,10 +29,15 @@ public class Road {
             return false;
         }
     }
-
+/**
+Creacion del metodo getNumberOfVehicles
+*/
     public int getNumberOfVehicles() {
         return numberOfVehicles;
     }
+/**
+Creacion del metodo get Vehicle
+*/
 
     public Vehicle getVehicle(int i) {
         if (i < Vehicles.length) {
@@ -36,4 +48,8 @@ public class Road {
         }
     }
 }
-
+/**
+ Tomás Sastre Cámara
+ ASIX 2n
+ IAW
+*/
