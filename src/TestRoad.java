d50bacdaea9b805d5e01c573804253dac46d66d4
Tomás Sastre <32461851+tsc00@users.noreply.github.com>
2017-11-08 20:04:11 +0100

    Update TestRoad.java

--- src/TestRoad.java
+++ src/TestRoad.java
@@ -1,7 +1,11 @@
 package transport;
-
+/**
+Creacion de la Clase TestRoad almacenado en el package transport
+*/
 import javax.swing.plaf.synth.SynthEditorPaneUI;
-
+/**
+Creacion de carreteras, vehiculos con carga maxima y añadirlos a la carretera 
+*/
 public class TestRoad {
     static public void main(String args[]) {
         Road myRoad = new Road();
@@ -11,7 +15,9 @@ public class TestRoad {
         myRoad.addVehicle(20000);
         myRoad.addVehicle(15000);
         myRoad.addVehicle(4000);
-
+/**
+Visualizar el vehiculos numero 4 y añadirle carga
+*/
         Vehicle Truck = myRoad.getVehicle(4);
         boolean peso = Truck.addBox(10000);
         System.out.println(peso);
@@ -21,3 +27,8 @@ public class TestRoad {
         System.out.println("The total truck load number 4 is : " + Truck.getLoad() +  " KG");
     }
 }
+/**
+ Tomás Sastre Cámara
+ ASIX 2n
+ IAW
+*/
